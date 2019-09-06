503dc066977427e87015dfe0eb676814c9db100a
abdhus4 <abdhus4@5CD7172DYF.thn.local>
2019-09-06 10:09:26 +0200

    Start

---
+++ prog1abdikadir/src/prog1abdikadir/Test1.java
@@ -0,0 +1,32 @@
+package prog1abdikadir;
+
+public class Test1 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+
+	int 	y = 1; // tilldelning
+
+		int tal = 1; // deklarering + tilldelning = initsiering
+		int Tal = 20; // integers heltal
+
+		double tal3= 2.0000000;
+		String  mening="hej sfdjhfh dfbhddhf dfbdh";
+		char ettTecken= '\u00FC';
+
+		boolean logik= false;
+
+		int antalhjul=2, antalvexlar=3; // flera variabler med samma datatyp
+		
+		System.out.println(ettTecken);
+
+
+		System.out.println("dfd");
+	}
+	
+}
